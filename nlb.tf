8a3b9fe4aeb9aeab598b34da8a6c6d3e700cf975
EC2 Default User <ec2-user@ip-172-31-36-166.ec2.internal>
2022-07-19 11:46:31 +0000

    Final AWS Terraform

--- nlb.tf
+++ nlb.tf
@@ -10,13 +10,27 @@ resource "aws_lb" "epsilon-lb" {
 }
 resource "aws_lb_listener" "epsilon-lb" {
   load_balancer_arn = aws_lb.epsilon-lb.arn
-  port              = "80"
-  protocol          = "TCP"
+  port              = "443"
+  protocol          = "TLS"
+  certificate_arn   = aws_acm_certificate_validation.sba_react_cert_validation.certificate_arn
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.epsilon-tg.arn
   }
 }
+resource "aws_lb_listener" "epsilon-lb-http" {
+  load_balancer_arn = aws_lb.epsilon-lb.arn
+  port              = "80"
+  protocol          = "TCP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.epsilon-tg.arn
+  }
+}
+
+  
 resource "aws_lb_target_group" "epsilon-tg" {
   name     = "epsilon-tg"
   port     = 80
@@ -35,8 +49,21 @@ resource "aws_lb" "epsilon-lb-api" {
 }
 resource "aws_lb_listener" "epsilon-lb-api" {
   load_balancer_arn = aws_lb.epsilon-lb-api.arn
+  port              = "443"
+  protocol          = "TLS"
+  certificate_arn   = aws_acm_certificate_validation.sba_api2_cert_validation.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.epsilon-tg-api.arn
+  }
+}
+
+resource "aws_lb_listener" "epsilon-lb-api-http" {
+  load_balancer_arn = aws_lb.epsilon-lb-api.arn
   port              = "80"
   protocol          = "TCP"
+  
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.epsilon-tg-api.arn
@@ -52,4 +79,87 @@ resource "aws_lb_target_group" "epsilon-tg-api" {
 
 output "App_Endpoint" {
   value = "${aws_lb.epsilon-lb.dns_name}"
+}
+
+resource "aws_acm_certificate" "sba_react_cert" {
+  domain_name       = "epsilon-smartbankapp.cloudtech-training.com"
+  validation_method = "DNS"
+}
+
+data "aws_route53_zone" "sba_zone" {
+  name         = "cloudtech-training.com"
+  private_zone = false
+}
+
+resource "aws_route53_record" "sba_react_zone_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.sba_react_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = false
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.sba_zone.zone_id
+}
+
+resource "aws_route53_record" "sba_react_zone_a_record" {
+  allow_overwrite = false
+  zone_id         = data.aws_route53_zone.sba_zone.zone_id
+  name            = "epsilon-smartbankapp.cloudtech-training.com"
+  type            = "A"
+
+  alias {
+    name                   = aws_lb.epsilon-lb.dns_name
+    zone_id                = aws_lb.epsilon-lb.zone_id
+    evaluate_target_health = true
+  }
+}
+resource "aws_acm_certificate_validation" "sba_react_cert_validation" {
+  certificate_arn         = aws_acm_certificate.sba_react_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.sba_react_zone_record : record.fqdn]
+}
+# API Certificate
+resource "aws_acm_certificate" "sba_api2_cert" {
+  domain_name       = "epsilon-smartbankapi.cloudtech-training.com"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "sba_api2_zone_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.sba_api2_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = false
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.sba_zone.zone_id
+}
+
+resource "aws_route53_record" "sba_api2_zone_a_record" {
+  allow_overwrite = false
+  zone_id         = data.aws_route53_zone.sba_zone.zone_id
+  name            = "epsilon-smartbankapi.cloudtech-training.com"
+  type            = "A"
+
+  alias {
+    name                   = aws_lb.epsilon-lb-api.dns_name
+    zone_id                = aws_lb.epsilon-lb-api.zone_id
+    evaluate_target_health = true
+  }
+}
+resource "aws_acm_certificate_validation" "sba_api2_cert_validation" {
+  certificate_arn         = aws_acm_certificate.sba_api2_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.sba_api2_zone_record : record.fqdn]
 }
